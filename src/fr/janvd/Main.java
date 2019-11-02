c849b8af7a4f91724d13e9928dfdec5e9599542c
janvandan <jan@vandravik.com>
2019-11-02 18:40:44 +0100

    Version calculatrice intiale

--- src/fr/janvd/Main.java
+++ src/fr/janvd/Main.java
@@ -3,6 +3,10 @@ package fr.janvd;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Mes Premiers pas en Java");
+        System.out.println("L'addition de 4 et 3 est : " + additionner(4, 3));
+    }
+
+	public static int additionner(int nombre1, int nombre2) {
+	    return nombre1 + nombre2;
     }
 }
