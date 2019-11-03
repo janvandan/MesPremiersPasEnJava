2db6b204221ee41b890ec7e9eec276e4e2fa39ad
janvandan <jan@vandravik.com>
2019-11-03 13:28:14 +0100

    Test pour la Version calculatrice intiale

---
+++ test/fr/janvd/MainTest.java
@@ -0,0 +1,14 @@
+package fr.janvd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void additionner() {
+        int resutat = Main.additionner(1,2);
+        assertEquals(3,resutat);
+    }
+}
