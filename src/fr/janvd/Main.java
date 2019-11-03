8a08d40592a420373a7e6c56b2afa88f34158281
janvandan <jan@vandravik.com>
2019-11-03 15:09:33 +0100

    Test pour la Version calculatrice intiale addition et soustraction

--- src/fr/janvd/Main.java
+++ src/fr/janvd/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("L'addition de 4 et 3 est : " + additionner(4, 3));
+        System.out.println("La soustraction entre 4 et 3 est : " + soustraire(4,3));
     }
 
-	public static int additionner(int nombre1, int nombre2) {
+	static int additionner(int nombre1, int nombre2) {
 	    return nombre1 + nombre2;
+    }
+
+    static int soustraire(int nombre1, int nomrebre2) {
+        return nombre1 - nomrebre2;
     }
 }
